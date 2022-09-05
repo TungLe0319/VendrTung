658b0db8550dccf6320fa75f9c9dfc1773cea49b
tungle0319 <107594367+tungle0319@users.noreply.github.com>
2022-09-05 13:59:59 -0600

    1234

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-export class Snack {
-  constructor(name, price) {
-    this.name = name;
-    this.price = price;
-  }
-}
+// export class Snack {
+//   constructor(name, price) {
+//     this.name = name;
+//     this.price = price;
+//   }
+// }
 
-snacks = [
-  new Snack('Fresh Mochi', 1.25),
-  new Snack('Boba Tea', 4.5),
-  new Snack('Sushi Platter', 25.5),
-  new Snack('Fresh Mochi', 1.25),
-  new Snack('Fresh Mochi', 1.25),
-];
+// snacks = [
+//   new Snack('Fresh Mochi', 1.25),
+//   new Snack('Boba Tea', 4.5),
+//   new Snack('Sushi Platter', 25.5),
+//   new Snack('Fresh Mochi', 1.25),
+//   new Snack('Fresh Mochi', 1.25),
+// ];
 
 let moneyDOM = document.getElementById('money');
 let sushiPlatter$Dom = document.getElementById('sushiPlatter');
 let pokeBowl$DOM = document.getElementById('pokeBowl');
+
+let you = {name:'you', hair:true, friends:[]}
+let them = {name:'them', hair:false, friends:[]}
+
+you.friends.push(them)
+console.log(you);
